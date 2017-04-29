34428d81486ef2ca9575089da51a094938cb18f4
Aleksey Kornev <aleksei.kornev@gmail.com>
2017-04-29 12:31:27 -0400

    Implement create/update model and form parser

--- src/gform/controllers/controllers.go
+++ src/gform/controllers/controllers.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
     "net/http"
+    "net/html"
+    "net/html/atom"
 	"github.com/corneldamian/httpway"
     "gform/models"
+    "github.com/yhat/scrape"
 )
 
 
@@ -14,12 +17,66 @@ func Index(w http.ResponseWriter, r *http.Request) {
     ctx.Set("status", 200)
 }
 
-func StoreForm(s models.Storage) httpway.Handler {
+func StoreForm(s *models.Storage) httpway.Handler {
     return func(w http.ResponseWriter, r *http.Request) {
+        // get google form url from user
+        gformUrl:=r.FormValue("url")
+
+        // get google form
+        resp, err:=http.Get(gformUrl)
+        if err != nil {
+            // TODO: error need to render template
+            http.Error(w, "Couldn't download google form", http.BadRequest)
+            return
+        }
+
+        // parse html
+        gformPage, err:=html.Parse(resp.Body)
+        if err != nil {
+            // TODO: error need to render template
+            http.Error(w, "Couldn't parse html of google form", http.BadRequest)
+            return
+        }
+
+        // try to find <form> node
+        gformMatcher:=func(n *html.Node) bool {
+            return n != nil && n.DataAtom == atom.Form
+        }
+        gform, ok:=scrape.Find(gformPage, formMatcher)
+        if !ok {
+            // TODO: error need to render template
+            http.Error(w, "Couldn't parse html of google form", http.BadRequest)
+            return
+        }
+
+        form:=models.Form{}
+        form.PageUrl = gformUrl
+        form.Url = scrape.Attr(gform, "action")
+        form.method = scrape.Attr(gform, "method")
+
+        gformInputMatcher:= func (n *html.Node) bool {
+            return n != nil && n.DataAtom == atom.Input
+        }
+        gformInputs,ok:=scrape.FindAll(gform)
+        if !ok {
+            // TODO: error need to render template
+            http.(w, "Couldn't parse html of google form", http.BadRequest)
+            return
+        }
+        for i, gformInput := range gformInputs {
+            field:=models.Field{}
+            field.FieldId=scrape.Attr(gformInput, "name")
+            field.Name=scrape.Attr(gformInput, "label")
+            field.MappedName=field.Name
+            append(form.Fields, field)
+        }
+        s.SaveForm(&form)
+
+        w.WriteHeader(200)
     }
 }
 
-func FormHandler(s models.Storage) httpway.Handler {
+func FormHandler(s *models.Storage) httpway.Handler {
     return func(w http.ResponseWriter, r *http.Request) {
     }
 }
--- src/gform/models/models.go
+++ src/gform/models/models.go
@@ -2,31 +2,39 @@ package models
 
 import (
     "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 type Field struct {
-    Id string
-    FieldId string
-    Name string
-    MappedName string // this can be equal name
+    FieldId string `json:"field_id" bson:"field_id"`
+    Name string `json:"name" bson:"name"`
+    MappedName string `json:"mapped_name" bson:"mapped_name"`// this can be equal name
 }
 
 type Form struct {
-    Id string
-    Url string
-    Fields []* Field
+    Id string `json:"id" bson:"_id,"`
+    PageUrl string `json:"page_url" bson:"page_url"`
+    Url string `json:"url" bson:"url"`
+    Fields []* Field `json:"fields" bson:"fields"`
+    Method string `json:"method" bson:"method"`
 }
 
 type Storage interface {
     GetForm(id string) (form *Form, e error)
     GetForms() (forms []*Form, e error)
-    SaveForm(form *Form) (r bool, e error)
-    DeleteForm(form *Form) (r bool, e error)
+    SaveForm(form *Form) (e error)
+    DeleteForm(form *Form) (e error)
 }
 
 type MongoStorage struct {
    Session *mgo.Session
 }
+
+const(
+    DB_NAME = "gform"
+    FORM_COLLECTION = "forms"
+)
+
 
 func (s MongoStorage) GetForm(id string) (form *Form, e error) {
     return nil, nil
@@ -36,10 +44,15 @@ func (s MongoStorage) GetForms() (form []*Form, e error) {
     return nil, nil
 }
 
-func (s MongoStorage) SaveForm(form *Form) (r bool, e error) {
-    return false, nil
+func (s MongoStorage) SaveForm(form *Form) (e error) {
+    var c = s.Session.DB(DB_NAME).C(FORM_COLLECTION)
+    if (form.Id == "") {
+        form.Id = bson.NewObjectId().Hex()
+        return c.Insert(form)
+    }
+    return c.UpdateId(form.Id, bson.M{"$set": form})
 }
 
-func (s MongoStorage) DeleteForm(form *Form) (r bool, e error) {
-    return false, nil
+func (s MongoStorage) DeleteForm(form *Form) (e error) {
+    return nil
 }
